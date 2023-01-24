daab10fadb0092eeda16ef6d4df9e27ec8b5a4f3
milessteamaccount <ronaldlamdev@gmail.com>
2023-01-23 21:59:09 -0600

    files for project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/about', (request, response) => {
+  response.sendFile(__dirname + '/about.html')
+})
+
+app.get('/contact-me', (request, response) => {
+  response.sendFile(__dirname + '/contact-me.html')
+})
+
+app.use((request, response) => {
+  response.sendFile(__dirname + '/404.html')
+})
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080")
+});
