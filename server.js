dbe0c979f964a089803c03bb952027dbfe2856c5
milessteamaccount <ronaldlamdev@gmail.com>
2023-01-24 21:51:45 -0600

    added dirname

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  response.sendFile('./index.html')
+  response.sendFile(__dirname + '/index.html')
 })
 
 app.get('/about', (request, response) => {
-  response.sendFile('./about.html')
+  response.sendFile(__dirname + '/about.html')
 })
 
 app.get('/contact-me', (request, response) => {
-  response.sendFile('./contact-me.html')
+  response.sendFile(__dirname+ '/contact-me.html')
 })
 
 app.use((request, response) => {
-  response.sendFile('./404.html')
+  response.sendFile(__dirname + '/404.html')
 })
 
 app.listen(8080, () => {
